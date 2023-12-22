96c32ddf4f1528012ecd76abb50cb71b7bef2f19
Lucas Glaydson <lucas.glaydson01@hotmail.com>
2023-12-22 19:08:35 -0300

    Desafio POO feito com sucesso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("1234567", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("2131", "Modelo 2", "22222222", 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
